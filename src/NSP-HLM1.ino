2b4fee5bebea707868188f5a64deb238831b8a02
Francisco Lobo <flobo@lobolabs.com>
2017-11-05 23:51:29 -0600

    Improved threading, added satcom interpreted, overall improvements to code

--- src/NSP-HLM1.ino
+++ src/NSP-HLM1.ino
@@ -6,9 +6,12 @@
  */
 
 #include "TinyGPS++.h"
+#include "Serial5/Serial5.h"
 
 //MISSION SETTINGS
-bool cellModemEnabled = false;
+bool cellModemEnabled = true;
+bool satModemEnabled = false;
+
 float altitudeGainClimbTrigger = 20;
 float altitudePerMinuteGainClimbTrigger = 5;
 float altitudeLossPerMinuteForDescentDetection = 20;
@@ -20,6 +23,7 @@ float minimumAltitudeToTriggerRecovery = 2300;
 bool simulationMode = false;
 bool debugMode = true;
 bool gpsDebugDump = false;
+bool satDebugDump = false;
 float simulatedApogeeAltitude = 1000;
 //
 
@@ -31,11 +35,13 @@ SYSTEM_THREAD(ENABLED);
 #define COMPUTER Serial
 #define GPSEvent serialEvent1
 #define computerEvent serialEvent
+#define SATCOM Serial5
+#define SATCOMEvent serialEvent5
 
+#define SATCOMEnablePin D2
 
 Timer timer(1000, second_tick);
 int elapsedSeconds = 0;
-int elapsedMinutes = 0;
 
 int recoveryDetectionIterations = 0;
 
@@ -50,15 +56,21 @@ float simulatedAltitude = 0.0; //For simulation only.
 bool simulatedClimbEnded = false;
 
 FuelGauge fuel;
-
 float batteryLevel = -1.0;
 
+int satcomSignal = -1;
+bool satcomAlive = false;
+
+int cellSignalRSSI = -1;
+int cellSignalQuality = -1;
+
 String computerSerialData;
+String satSerialData;
+
 
 enum MissionStage { 
 	ground,
-	climb,
-	apogee,
+	climb,	
 	descent,
 	recovery
 };
@@ -81,89 +93,98 @@ bool success = Particle.function("c", computerRequest);
 
 
 void setup() {
-	RGB.control(true);
+	//TAKE CONTROL OF THE RGB LED ONBOARD
+	RGB.control(true); 
+	//Setup SATCOM
+	pinMode(SATCOMEnablePin, OUTPUT);	
+	digitalWrite(SATCOMEnablePin, HIGH);
+	//CONNECT TO GPS
 	GPS.begin(57600);
-	Serial.begin(57600);
+	//CONNECT TO COMPUTER VIA USB
+	COMPUTER.begin(57600);	
+	//SETUP BATTERY CHARGER 
 	setupBatteryCharger();	
+	//CHANGE LED COLOR
 	RGB.color(0, 0, 255);
 	RGB.brightness(100);	
+	//Set Cell Modem depending on the initial setup configuration (See CellModemEnable var at start of code)
 	setCellModem(cellModemEnabled);
-
+	//WAIT 2 seconds to start
 	delay(2000);
 	RGB.color(255, 255, 255);
 	RGB.brightness(0);
-	timer.start();
-	batteryLevel = fuel.getSoC();
-	Serial.println("[Stage] Ground ");
-
+	//BEGIN SatCom Comunications (delay further to allow super cap to charge);
+	SATCOM.begin(19200, SERIAL_8N1);
+	setSatModem(satModemEnabled);
+	//Start the basic timed events
+	timer.start();	
+	//READ INITIAL POWER LEFT In Battery
+	batteryLevel = fuel.getSoC();	
+	sendToComputer("[Stage] Ground ");	
 
 }
 
-void loop() {}
+void loop() {} //
 
 
 //TIMED EVENTS
+//This will happen every second.
 void second_tick() {
 	timer.stopFromISR();
 	elapsedSeconds++;
-
-
+	
 	if (missionStage == ground && Cellular.ready() == false) { RGB.color(255,165,0); } //Orange
-	if (missionStage == ground && Cellular.connecting() == true) { RGB.color(50,50,255); } //Orange
-	if (missionStage == ground && Cellular.ready() == true) { RGB.color(100,165,100); } //Orange
+	if (missionStage == ground && Cellular.connecting() == true) { RGB.color(50,50,255); } //Whitish
+	if (missionStage == ground && Cellular.ready() == true) { RGB.color(0,255,0); } //Green
 	if (missionStage == climb) { RGB.color(0,0,255); } //Blue
-	if (missionStage == apogee) { RGB.color(0,255,0); } //Green
 	if (missionStage == descent) { RGB.color(255,215,0); } //Yellow
 	if (missionStage == recovery) { RGB.color(255,0,255); } //Magenta
 
-	if (elapsedSeconds % 2 == 0 ) {
+	if (elapsedSeconds % 2 == 0 ) { //Pulse LED every 2 seconds
 		RGB.brightness(0);
 	} else {
 		if (debugMode == true) {
 			RGB.brightness(100);
 		}
 	}
-
-	if (elapsedSeconds==30) { a30second_tick(); }
-	if (elapsedSeconds==60) { a60second_tick(); }
-
-	if (elapsedSeconds>=60) { elapsedSeconds = 0; }
-
-	//Second Updates////////////	
-	updateStage();
-	/////////////////
 	
-	if (debugMode == true && gpsDebugDump == false) {
-		Serial.println(satString());
+
+	if (elapsedSeconds % 9 == 0) {		
+		if (satModemEnabled == true && satcomAlive == false) {			
+			SatPing();
+		}
+
+		if (satModemEnabled == true && satcomAlive == true) {			
+			getSatSignal();	
+		}			
 	}
 
 
+	if (elapsedSeconds % 32 == 0) {
+		getSatSignal();	
+		sendStatusToCloud(); //Send SAT STRING to cloud if connected..		
+	}
+
+	if (elapsedSeconds % 45 == 0) {
+		a60second_tick();
+		sendStatusToSat();	
+		batteryLevel = fuel.getSoC(); 		
+		elapsedSeconds = 0;
+	}
+ 	
+
+	//Second Updates////////////	(State machine manager will update the stage)
+	updateStage();
+	/////////////////
+
+	if (debugMode == true && gpsDebugDump == false && satDebugDump == false) {		
+		sendToComputer(satString());		
+	}
+
+	
+
 	timer.startFromISR();		
 }
-
-
-void a30second_tick() {	
-	// Serial.println(satString());	
-}
-
-void a60second_tick() {
-	elapsedMinutes++;
-	if (elapsedMinutes==3) { a3Minute_tick();  }
-	if (elapsedMinutes>=5) { a5Minute_tick(); elapsedMinutes = 0; }
-
-	Serial.println("60 TICK");
-	batteryLevel = fuel.getSoC();
-}
-
-void a3Minute_tick() {
-	Serial.println("[EVENT] 3 Minute Tick");
-	sendStatusToCloud(); //Send SAT STRING to cloud if connected..
-}
-
-void a5Minute_tick() {
-	Serial.println("[EVENT] 5 Minute Tick");
-}
-
 
 
 //State Updater
@@ -174,8 +195,9 @@ void updateStage() {
 	float altitudeTrend = gpsAltitude - lastGPSAltitude;	
 	altitudePerMinute = altitudeTrend * 60;
 	
-	if ((gpsAltitude < lastGPSAltitude) && (gpsAltitude > altitudeOfApogee) && lastGPSAltitude != -1)  {
-		Serial.println("[Event] Apogee Reached at " + String(gpsAltitude));
+	if ((gpsAltitude < lastGPSAltitude) && (gpsAltitude > altitudeOfApogee) && lastGPSAltitude != -1)  {		
+		sendToComputer("[Event] Apogee Reached at " + String(gpsAltitude));
+		
 		altitudeOfApogee = gpsAltitude;
 	}
 
@@ -202,25 +224,22 @@ void updateStage() {
 	//State Machine
 	if ((missionStage == ground && initialGPSAltitude > 0) && (altitudeGain > altitudeGainClimbTrigger) && (altitudePerMinute > altitudePerMinuteGainClimbTrigger)) {
 		missionStage = climb;
-		lastPositiveGPSAltitude = gpsAltitude;
-		Serial.println("[Stage] Climb Detected at: " + String(gpsAltitude));
+		lastPositiveGPSAltitude = gpsAltitude;		
+		sendToComputer("[Stage] Climb Detected at: " + String(gpsAltitude));		
 	}
 
-	if (missionStage == climb && altitudeOfApogee != -1) {
-		missionStage = apogee;
-		Serial.println("[Stage] Apogee Detected at " + String(altitudeOfApogee));
-	}
 
-	if (missionStage == apogee && altitudeTrend < 0 && altitudePerMinute <= altitudeLossPerMinuteForDescentDetection) {
-		missionStage = descent;
-		Serial.println("[Stage] Descent Detected at " + String(gpsAltitude));
+	if (missionStage == climb && altitudeTrend < 10 && altitudePerMinute <= altitudeLossPerMinuteForDescentDetection) {
+		missionStage = descent;		
+		sendToComputer("[Stage] Descent Detected at " + String(gpsAltitude));
+	
 	}
 
 	if ((missionStage == descent && altitudePerMinute < 2) && (gpsAltitude <= minimumAltitudeToTriggerRecovery)) {
 		recoveryDetectionIterations++;
 		if (recoveryDetectionIterations >= iterationsInLowDescentToTriggerRecovery) {			
-			missionStage = recovery;
-			Serial.println("[Stage] Recovery Detected at " + String(gpsAltitude));	
+			missionStage = recovery;			
+			sendToComputer("[Stage] Recovery Detected at " + String(gpsAltitude));				
 		}
 	} 
 
@@ -230,9 +249,13 @@ void updateStage() {
 }
 
 void sendStatusToCloud() {
-	if (Particle.connected() == true) { 
-		Particle.publish("S",satString());
+	if (Particle.connected() == true) { 		
+		Particle.publish("S",satString());		
 	}
+}
+
+void sendStatusToSat() {	
+	sendTextToSat(satString());
 }
 
 //Helper Functions
@@ -246,14 +269,14 @@ String satString() {
    String(gpsParser.satellites.value()) + "," + 
    String(gpsParser.hdop.value()) +  "," +    
    String(batteryLevel/10,0) +  "," + 
+   String(satcomSignal) +  "," +    
    missionStageShortString();
 
 }
 
 String missionStageShortString() {
 	if (missionStage == ground)  { return "G"; }
-	if (missionStage == climb)  { return "C"; }
-	if (missionStage == apogee)  { return "A"; }
+	if (missionStage == climb)  { return "C"; }	
 	if (missionStage == descent)  { return "D"; }
 	if (missionStage == recovery)  { return "R"; }
 }
@@ -265,6 +288,32 @@ void setupBatteryCharger() {
 }
 
 
+void SATCOMEvent() {	
+		while (SATCOM.available()) {			
+			char c = SATCOM.read();	
+			satSerialData = satSerialData + c;								
+			if (satDebugDump==true && gpsDebugDump==false) {
+				TRY_LOCK(COMPUTER) {
+					COMPUTER.write(c);
+				}
+			}
+			if (c == '\r') {				
+				satSerialData = satSerialData.trim();
+				if (satSerialData.substring(0,2) == "OK" && satcomAlive == false) {
+					satcomAlive = true;					
+					sendToComputer("[Event] SatCom Alive");					
+					getSatSignal();
+				}								
+				if (satSerialData.substring(0,5) == "+CSQ:") {
+					String signal = satSerialData.substring(5);
+					satcomSignal = signal.toInt();										
+				}
+				
+				satSerialData = "";
+			}
+		}	
+}
+
 // LISTENING EVENTS
 void GPSEvent()
 {	
@@ -272,18 +321,19 @@ void GPSEvent()
 		while (GPS.available()) {			
 			char c = GPS.read();			
 			gpsParser.encode(c);
-			if (gpsDebugDump==true) {
-				Serial.print(c);  
-			}
-			
+			if (gpsDebugDump==true && satDebugDump==false) {
+				TRY_LOCK(COMPUTER) {
+					COMPUTER.print(c);  
+				}
+			}			
 		}
 	}	
 
 	if (gpsParser.hdop.value() < 300) {
 		gpsState = Fix;		
 			if (initialGPSAltitude==-1 && gpsParser.altitude.feet() > 0) {
-				initialGPSAltitude = gpsParser.altitude.feet();	
-				Serial.println("[Event] Initial Altitude Set to: " + String(initialGPSAltitude,0));
+				initialGPSAltitude = gpsParser.altitude.feet();					
+				sendToComputer("[Event] Initial Altitude Set to: " + String(initialGPSAltitude,0));				
 			}
 	}	else {
 		gpsState = noFix;		
@@ -306,26 +356,27 @@ void computerEvent()
 	}	
 }
 
+
 int computerRequest(String param) {	
 	
 	if  (param == "deboff") {
 		debugMode = false;
-		COMPUTER.println("OK");
+		sendToComputer("OK");
 		return 1;
 	}
 	if  (param == "debon") {
 		debugMode = true;
-		COMPUTER.println("OK");
+		sendToComputer("OK");
 		return 1;
 	}
 	if  (param == "simon") {
 		simulationMode = true;
-		COMPUTER.println("OK");
+		sendToComputer("OK");
 		return 1;
 	}
 	if  (param == "simoff") {
 		simulationMode = false;
-		COMPUTER.println("OK");
+		sendToComputer("OK");
 		return 1;
 	}
 	if  (param == "reset") {
@@ -337,14 +388,14 @@ int computerRequest(String param) {
 		altitudePerMinute = 0.0;
 		altitudeOfApogee = -1.0;
 		simulatedAltitude = 0.0; //For simulation only.
-		COMPUTER.println("OK");
+		sendToComputer("OK");
 		return 1;
 	}
 	if (param == "reboot") {
+		sendToComputer("OK");
 		System.reset();		
 	}
-	if (param == "cellon") {		
-		COMPUTER.println("OK");
+	if (param == "cellon") {				
 		setCellModem(true);		
 		return 1;
 	}
@@ -352,94 +403,154 @@ int computerRequest(String param) {
 		setCellModem(false);
 		return 1;
 	}
+	if (param == "saton") {				
+		setSatModem(true);		
+		return 1;
+	}
+	if (param == "satoff") {		
+		setSatModem(false);
+		return 1;
+	}
+	if (param == "comon") {				
+		setSatModem(true);	
+		setCellModem(true);	
+		return 1;
+	}
+	if (param == "comoff") {		
+		setSatModem(false);
+		setCellModem(false);
+		return 1;
+	}
 	if (param == "gpsdump") {		
 		gpsDebugDump = !gpsDebugDump;
 		return 1;
 	}
+	if (param == "satdump") {		
+		satDebugDump = !satDebugDump;
+		return 1;
+	}
+	if (param == "querysatsignal") {		
+		sendToComputer("OK");
+		getSatSignal();
+		return 1;
+	}	
+	if (param == "querycellsignal") {		
+		sendToComputer("OK");
+		getCellSignal();
+		return 1;
+	}
 	if  (param == "vsi?") {
-		COMPUTER.println(String(altitudePerMinute));		
+		sendToComputer(String(altitudePerMinute));		
 	}
 	if  (param == "alt?") {
-		COMPUTER.println(String(lastGPSAltitude));		
+		sendToComputer(String(lastGPSAltitude));		
 	}
 	if  (param == "apogee?") {
-		COMPUTER.println(String(altitudeOfApogee));
+		sendToComputer(String(altitudeOfApogee));
 	}
 	if  (param == "stage?") {
-		COMPUTER.println(missionStageShortString());
+		sendToComputer(missionStageShortString());
 	}	
 	if  (param == "cell?") {
 		if (Cellular.ready() == true) { 
-			COMPUTER.println("YES");
+			sendToComputer("YES");
 		} else {
-			COMPUTER.println("NO");
+			sendToComputer("NO");
 		}
 	}	
 	if  (param == "cellconnecting?") {
 		if (Cellular.connecting() == true) { 
-			COMPUTER.println("YES");
+			sendToComputer("YES");
 		} else {
-			COMPUTER.println("NO");
+			sendToComputer("NO");
 		}
 	}
-	if  (param == "rssi?") {
+	if  (param == "rssi?") {		
 		CellularSignal sig = Cellular.RSSI();		
-		Serial.println(sig);
-
+		sendToComputer(sig);
 	}	
 
 	if  (param == "cloud?") {
 		if (Particle.connected() == true) { 
-			COMPUTER.println("YES");
+			sendToComputer("YES");
 		} else {
-			COMPUTER.println("NO");
+			sendToComputer("NO");
 		}
 	}	
+	
+	if  (param == "satsignal?") {
+		sendToComputer(satcomSignal);
+		return satcomSignal;
+	}	
+
+	if  (param == "satenabled?") {
+		if (satModemEnabled) {
+			sendToComputer("YES");
+			return 1;
+		}
+		sendToComputer("NO");
+		return false;
+	}
 
 	if (param == "fwversion?") {
-		COMPUTER.printlnf("Firmware version: %s", System.version().c_str());
+		TRY_LOCK(COMPUTER) {
+			COMPUTER.printlnf("Firmware version: %s", System.version().c_str());
+		}
 	}
 
 	if (param == "$") {
-		COMPUTER.println(satString());		
+		sendToComputer(satString());		
 	}
 
-	if (param == "$$") {
-		COMPUTER.println(satString());
+	if (param == "$$") {	
+		sendToComputer("OK");	
 		sendStatusToCloud();		
 	}
 
-	if (param == "$$$") {
-		//TODO
+	if (param == "$$$") {	
+		sendToComputer("OK");
+		sendStatusToSat();		
 	}
 
+	
+
 	if (param == "?") {
-		COMPUTER.printlnf("-------------------------.--------------------------");		
-		COMPUTER.printlnf("Status Sentence (1hz):");
-		COMPUTER.printlnf("TIME,LAT,LON,ALT,SPEED,COURSE,SATS,HDOP,BATT,STAGE");		
-		COMPUTER.printlnf("-------------------------.--------------------------");		
-		COMPUTER.printlnf("deboff = Debug Off");
-		COMPUTER.printlnf("debon = Debug On");
-		COMPUTER.printlnf("simon = Start Simulation");
-		COMPUTER.printlnf("simoff = Stop Simulation");
-		COMPUTER.printlnf("reset = Set mission to ground mode");
-		COMPUTER.printlnf("reboot = Reboot Flight Computer");
-		COMPUTER.printlnf("simon = Start Simulation");
-		COMPUTER.printlnf("cellon = Cell Modem On");
-		COMPUTER.printlnf("celloff = Cell Modem Off");
-		COMPUTER.printlnf("celloff = Cell Modem Off");
-		COMPUTER.printlnf("gpsdump = GPS Serial Dump to computer toggle");
-		COMPUTER.printlnf("vsi? = Vertical Speed?");
-		COMPUTER.printlnf("alt? = Altitude in feet?");
-		COMPUTER.printlnf("cell? = Cell Status?");
-		COMPUTER.printlnf("cellconnecting? = Cell Modem attempting to connect?");
-		COMPUTER.printlnf("rssi? = Cell Signal Strength?");
-		COMPUTER.printlnf("cloud? = Is cloud available?");
-		COMPUTER.printlnf("fwversion? = OS Firmware Version?");		
-		COMPUTER.printlnf("$ = Print status string");		
-		COMPUTER.printlnf("$$ = Print and send to cell cloud status string");		
-		COMPUTER.printlnf("$$$ = Print and send to cell cloud status string");		
-		COMPUTER.printlnf("-------------------------.--------------------------");
+		TRY_LOCK(COMPUTER) {
+		COMPUTER.println("-------------------------.--------------------------");		
+		COMPUTER.println("Status Sentence (1hz):");
+		COMPUTER.println("TIME,LAT,LON,ALT,SPEED,COURSE,SATS,HDOP,BATT,SAT,STAGE");
+		COMPUTER.println("-------------------------.--------------------------");		
+		COMPUTER.println("deboff = Debug Off");
+		COMPUTER.println("debon = Debug On");
+		COMPUTER.println("simon = Start Simulation");
+		COMPUTER.println("simoff = Stop Simulation");
+		COMPUTER.println("reset = Set mission to ground mode");
+		COMPUTER.println("reboot = Reboot Flight Computer");
+		COMPUTER.println("simon = Start Simulation");
+		COMPUTER.println("cellon = Cell Modem On");
+		COMPUTER.println("celloff = Cell Modem Off");
+		COMPUTER.println("saton = SAT Modem ON");
+		COMPUTER.println("satoff = SAT Modem Off");
+		COMPUTER.println("comoff = All Comunication systems OFF [cell + sat]");
+		COMPUTER.println("comon = All Comunication systems ON [cell + sat]");
+		COMPUTER.println("gpsdump = GPS Serial Dump to computer toggle");
+		COMPUTER.println("satdump = SATCOM Serial Dump to computer toggle");
+		COMPUTER.println("querysatsignal = Send a request to the satelite modem to get sat signal");
+		COMPUTER.println("querycellsignal = Send a request to the cellular modem to get RSSI signal");
+		COMPUTER.println("vsi? = Vertical Speed?");
+		COMPUTER.println("alt? = Altitude in feet?");
+		COMPUTER.println("cell? = Cell Status?");
+		COMPUTER.println("cellconnecting? = Cell Modem attempting to connect?");
+		COMPUTER.println("rssi? = Cell Signal Strength?");
+		COMPUTER.println("cloud? = Is cloud available?");
+		COMPUTER.println("satsignal? = 0-5 Satcom signal strength?");		
+		COMPUTER.println("satenabled? = Is the sat modem enabled?");		
+		COMPUTER.println("fwversion? = OS Firmware Version?");		
+		COMPUTER.println("$ = Print status string");		
+		COMPUTER.println("$$ = Print and send to cell cloud status string");		
+		COMPUTER.println("$$$ = Print and send to SAT cloud status string");		
+		COMPUTER.println("-------------------------.--------------------------");
+		}
 
 	}
 
@@ -449,14 +560,59 @@ int computerRequest(String param) {
 void setCellModem(bool value) {		
 	cellModemEnabled = value;
 	if ((cellModemEnabled == true) && (Cellular.ready() == false) && (Cellular.connecting() == false)) {
-		Cellular.on();
-		Cellular.connect();
-		Particle.connect();
-		Serial.println("[Event] Cell Modem ON");
+		// ATOMIC_BLOCK() {		
+			Cellular.on();
+			Cellular.connect();
+			Particle.connect();			
+		// }
+		sendToComputer("[Event] Cell Modem ON");
+		getCellSignal();
 		// waitUntil(Particle.connected);
 		// Serial.println("[Event] Cloud ONLINE.");
 	} else if (cellModemEnabled == false) {
 		Cellular.off();
-		Serial.println("[Event] Cell Modem OFF");
+		cellSignalRSSI = -1;
+		cellSignalQuality = -1;
+		sendToComputer("[Event] Cell Modem OFF");
 	}
 }
+
+void getCellSignal() {
+	if (cellModemEnabled == false) { return; }
+		// SINGLE_THREADED_BLOCK() {
+		CellularSignal cellSignal = Cellular.RSSI();
+		cellSignalRSSI = cellSignal.rssi;
+		cellSignalQuality = cellSignal.qual;
+	// }
+}
+
+void setSatModem(bool value) {	
+	if (satModemEnabled == false && value == true) {  sendToComputer("[Event] SAT Modem ON"); }
+	satModemEnabled = value;	
+	if (satModemEnabled == false) {   satcomAlive = false; satcomSignal = -1; sendToComputer("[Event] SAT Modem OFF"); return; }	
+	SATCOM.println("AT&K0");
+	getSatSignal();	
+}
+
+void getSatSignal() {
+	if (satModemEnabled == false) { return; }				
+		SATCOM.println("AT+CSQ");	
+}
+
+void sendTextToSat(String text) {
+	if (satModemEnabled == false) { return; }			
+		SATCOM.println("AT+SBDWT=" + text);	
+}
+
+void SatPing() {	
+		SATCOM.println("AT");	
+}
+
+void sendToComputer(String text) {
+	TRY_LOCK(COMPUTER) {	
+		COMPUTER.println(text);
+	}
+}
+
+
+
